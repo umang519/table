7006484b906401bc1ae7533ba7e44b633bef8002
umang519 <umangprajapati19504@gmail.com>
2025-03-10 23:31:25 +0530

    changes

--- my-app/app/login/page.js
+++ my-app/app/login/page.js
@@ -32,7 +32,7 @@ export default function Login() {
     validationSchema: validationSchema,
     onSubmit: async (values) => {
       try {
-        const response = await fetch("http://localhost:5000/api/login", {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
