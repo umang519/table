b73930c521b530878dba00e165b703313f3c0da1
umang519 <umangprajapati19504@gmail.com>
2025-03-10 23:14:46 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Connect to MongoDB
 connectDB();
