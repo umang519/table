8f3411a1e44a616acf8f86837b4893c0696b3cf6
umang519 <umangprajapati19504@gmail.com>
2025-03-10 21:47:32 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Connect to MongoDB
 connectDB();
@@ -28,7 +28,7 @@ app.use(bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000", credentials: true }));
 
 // Routes
 app.use('/api', userRoutes);
