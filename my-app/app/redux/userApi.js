b1923a0ae2ee2d0b045158bff76c2bcbf01a204c
umang519 <umangprajapati19504@gmail.com>
2025-03-11 10:44:35 +0530

    changes

--- my-app/app/redux/userApi.js
+++ my-app/app/redux/userApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userApi = createApi({
   reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL + "/api" }),
   tagTypes: ["Users", "Admins"],
   endpoints: (builder) => ({
     getUsers: builder.query({
